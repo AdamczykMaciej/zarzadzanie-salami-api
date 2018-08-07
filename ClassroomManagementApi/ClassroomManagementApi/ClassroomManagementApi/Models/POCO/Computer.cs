a4eb50ad378dc912abf913ad5b5d20b6cd7a1892
Maciej Adamczyk <adamczyk.maciej01@gmail.com>
2018-08-07 13:07:47 +0200

    Translated, Updated WebAPI

---
+++ ClassroomManagementApi/ClassroomManagementApi/ClassroomManagementApi/Models/POCO/Computer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExample.Models
+{
+    public class Computer
+    {
+        public int IdKomputer { get; set; }
+        public int IdMonitor { get; set; }
+        public string Procesor { get; set; }
+        public string RAM { get; set; }
+        public string KartaGraficzna { get; set; }
+        public int IdMaszynaWirtualna { get; set; }
+    }
+}
---
+++ ClassroomManagementApi/ClassroomManagementApi/ClassroomManagementApi/Models/POCO/Software.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExample.Models
+{
+    public class Software
+    {
+        public int IdOprogramowanie { get; set; }
+        public string Nazwa { get; set; }
+    }
+}
