047fb28696bf2e6c9b02e3a4b63950d5d5c6f244
romanov1589 <35072304+romanov1589@users.noreply.github.com>
2018-08-14 12:37:55 +0200

    An empty react-project added

---
+++ zarzadzanie-salami-react/src/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export class Header extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App-header">
+          <h1>PJATK</h1>
+      </div>
+    );
+  }
+}
+
+
