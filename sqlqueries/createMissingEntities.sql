d59a89069379593a84ba317dd16dbf902e0b35c0
Maciej Adamczyk <adamczyk.maciej01@gmail.com>
2018-08-16 19:23:29 +0200

    Added an entity and resolved problems with sql queries, conflicts. FInally working. Created an instruction, steps how to execute queries. Checked

--- sqlqueries/createMissingEntities.sql
+++ sqlqueries/createMissingEntities.sql
@@ -1,86 +1,103 @@
 -- =============================================
 -- Author: Maciej Adamczyk
 -- Create date: 2018/08/03
--- Description: create missing entities for ZarzadzanieSalami
+-- Description: 
 -- =============================================
 
 --CREATE TABLES--
+USE [dziekanat_hash]
 -- Table: Komputer
-CREATE TABLE dziekanat_hash.dbo.Komputer (
+GO
+CREATE TABLE dbo.Komputer (
     IdKomputer int  NOT NULL IDENTITY(1,1),
     IdMonitor int  NOT NULL,
     Procesor varchar(100)  NOT NULL,
     RAM varchar(100)  NOT NULL,
     KartaGraficzna varchar(100)  NOT NULL,
-    IdMaszynaWirtualna int  NULL,
     CONSTRAINT Komputer_pk PRIMARY KEY  (IdKomputer)
 );
-
+GO
 -- Table: MaszynaWirtualna
-CREATE TABLE dziekanat_hash.dbo.MaszynaWirtualna (
+CREATE TABLE dbo.MaszynaWirtualna (
     IdMaszynaWirtualna int  NOT NULL IDENTITY(1,1),
     Nazwa varchar(50)  NOT NULL,
     CONSTRAINT MaszynaWirtualna_pk PRIMARY KEY  (IdMaszynaWirtualna)
 );
-
+GO
+-- Table: MaszynaWirtualnaKomputer
+CREATE TABLE dbo.MaszynaWirtualnaKomputer (
+    IdMaszynaWirtualna int  NOT NULL,
+    IdKomputer int  NOT NULL,
+    CONSTRAINT MaszynaWirtualnaKomputer_pk PRIMARY KEY  (IdMaszynaWirtualna,IdKomputer)
+);
+GO
 -- Table: Monitor
-CREATE TABLE dziekanat_hash.dbo.Monitor (
+CREATE TABLE dbo.Monitor (
     IdMonitor int  NOT NULL IDENTITY(1,1),
     RozmiarMonitora int  NOT NULL,
     CONSTRAINT Monitor_pk PRIMARY KEY  (IdMonitor)
 );
-
+GO
 -- Table: Oprogramowanie
-CREATE TABLE dziekanat_hash.dbo.Oprogramowanie (
+CREATE TABLE dbo.Oprogramowanie (
     IdOprogramowanie int  NOT NULL IDENTITY(1,1),
     Nazwa varchar(100)  NOT NULL,
     CONSTRAINT Oprogramowanie_pk PRIMARY KEY  (IdOprogramowanie)
 );
-
+GO
 -- Table: OprogramowanieKomputerow
-CREATE TABLE dziekanat_hash.dbo.OprogramowanieKomputerow (
+CREATE TABLE dbo.OprogramowanieKomputerow (
     IdKomputer int  NOT NULL,
     IdOprogramowanie int  NOT NULL,
-    CONSTRAINT OprogramowanieKomputerow_pk PRIMARY KEY  (IdKomputerow,IdOprogramowanie)
+    CONSTRAINT OprogramowanieKomputerow_pk PRIMARY KEY  (IdKomputer,IdOprogramowanie)
 );
-
+GO
 -- Table: RozkladSali
-CREATE TABLE dziekanat_hash.dbo.RozkladSali (
+CREATE TABLE dbo.RozkladSali (
     IdRozkladSali int  NOT NULL IDENTITY(1,1),
     NazwaRozkladuSali varchar(50)  NOT NULL,
     CONSTRAINT RozkladSali_pk PRIMARY KEY  (IdRozkladSali)
 );
-
+GO
+USE [dziekanat_hash]
+GO
 -- foreign keys
--- Reference: Komputer_MaszynaWirtualna (table: Komputer)
-ALTER TABLE dziekanat_hash.dbo.Komputer ADD CONSTRAINT Komputer_MaszynaWirtualna
-    FOREIGN KEY (IdMaszynaWirtualna)
-    REFERENCES dziekanat_hash.dbo.MaszynaWirtualna (IdMaszynaWirtualna);
-
 -- Reference: Komputer_Monitor (table: Komputer)
-ALTER TABLE dziekanat_hash.dbo.Komputer ADD CONSTRAINT Komputer_Monitor
+ALTER TABLE dbo.Komputer ADD CONSTRAINT Komputer_Monitor
     FOREIGN KEY (IdMonitor)
-    REFERENCES dziekanat_hash.dbo.Monitor (IdMonitor);
-
+    REFERENCES dbo.Monitor (IdMonitor);
+GO
 -- Reference: Komputer_OprogramowanieKomputerow (table: OprogramowanieKomputerow)
-ALTER TABLE dziekanat_hash.dbo.OprogramowanieKomputerow ADD CONSTRAINT Komputer_OprogramowanieKomputerow
-    FOREIGN KEY (IdKomputerow)
-    REFERENCES dziekanat_hash.dbo.Komputer (IdKomputer);
-
--- Reference: Sala_Komputer (table: Sala)
-ALTER TABLE dziekanat_hash.dbo.Sala ADD CONSTRAINT Sala_Komputer
+ALTER TABLE dbo.OprogramowanieKomputerow ADD CONSTRAINT Komputer_OprogramowanieKomputerow
     FOREIGN KEY (IdKomputer)
-    REFERENCES dziekanat_hash.dbo.Komputer (IdKomputer);
-	
--- Reference: Sala_RozkladSali (table: Sala)
-ALTER TABLE dziekanat_hash.dbo.Sala ADD CONSTRAINT Sala_RozkladSali
-	FOREIGN KEY (IdRozkladSali)
-	REFERENCES dziekanat_hash.dbo.RozkladSali (IdRozkladSali);
-
--- Reference: SpecjalistyczneOprogramowanie_OprogramowanieKomputerow (table: OprogramowanieKomputerow)
-ALTER TABLE dziekanat_hash.dbo.OprogramowanieKomputerow ADD CONSTRAINT Oprogramowanie_OprogramowanieKomputerow
+    REFERENCES dbo.Komputer (IdKomputer);
+GO
+-- Reference: MaszynaWirtualnaKomputer_Komputer (table: MaszynaWirtualnaKomputer)
+ALTER TABLE dbo.MaszynaWirtualnaKomputer ADD CONSTRAINT MaszynaWirtualnaKomputer_Komputer
+    FOREIGN KEY (IdKomputer)
+    REFERENCES dbo.Komputer (IdKomputer);
+GO
+-- Reference: MaszynaWirtualnaKomputer_MaszynaWirtualna (table: MaszynaWirtualnaKomputer)
+ALTER TABLE dbo.MaszynaWirtualnaKomputer ADD CONSTRAINT MaszynaWirtualnaKomputer_MaszynaWirtualna
+    FOREIGN KEY (IdMaszynaWirtualna)
+    REFERENCES dbo.MaszynaWirtualna (IdMaszynaWirtualna);
+GO
+-- Reference: Oprogramowanie_OprogramowanieKomputerow (table: OprogramowanieKomputerow)
+ALTER TABLE dbo.OprogramowanieKomputerow ADD CONSTRAINT Oprogramowanie_OprogramowanieKomputerow
     FOREIGN KEY (IdOprogramowanie)
-    REFERENCES dziekanat_hash.dbo.Oprogramowanie (IdOprogramowanie);
+    REFERENCES dbo.Oprogramowanie (IdOprogramowanie);
+GO
+-- Reference: Sala_Komputer (table: Sala)
+ALTER TABLE dbo.Sala ADD CONSTRAINT Sala_Komputer
+    FOREIGN KEY (IdKomputer)
+    REFERENCES dbo.Komputer (IdKomputer);
+
+GO
+-- Reference: Sala_RozkladSali (table: Sala)
+GO
+ALTER TABLE dbo.Sala ADD CONSTRAINT Sala_RozkladSali
+    FOREIGN KEY (IdRozkladSali)
+    REFERENCES dziekanat_hash.dbo.RozkladSali (IdRozkladSali);
 
 -- End of file.
 
